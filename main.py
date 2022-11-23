934f96cc1292807a969c87ef9d92fc0bde9544bf
Jebet812 <cynthiajebet08@gmail.com>
2022-11-23 10:18:34 +0300

    Modified

--- main.py
+++ main.py
@@ -2,5 +2,12 @@
 #   - bs4
 #   -requests
 
+import requests #http requests
 
+from bs4 import BeautifulSoup #web scrapping
+#Send email 
+import smtplib 
+#email body
+ from email.mime.multipart import MIMEMultipart 
+ 
   
