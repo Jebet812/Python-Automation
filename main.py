3d3cd3be688396fa38eb1d1bdc4643af4781b30a
Jebet812 <cynthiajebet08@gmail.com>
2022-11-23 10:20:01 +0300

    Modified

--- main.py
+++ main.py
@@ -5,9 +5,16 @@
 import requests #http requests
 
 from bs4 import BeautifulSoup #web scrapping
-#Send email 
+
+#send email 
 import smtplib 
 #email body
- from email.mime.multipart import MIMEMultipart 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText
+
+# system date and time manipulation
+import datetime
+
+
  
   
