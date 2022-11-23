a77b515d72950d1c419854dc4c5dc9af8a2eb256
Jebet812 <cynthiajebet08@gmail.com>
2022-11-23 11:25:18 +0300

    Modified

--- main.py
+++ main.py
@@ -23,8 +23,19 @@ content = ''
 
 def extract_news(url):
     print('Extracting Hacker News Stories...')
+    cnt = ''
     cnt +=('<b>HN Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get (url)
+    content = response.content
+    soup =  BeautifulSoup(content, 'html.parser')
+    for i, tag in enumerate(soup.find_all('td', attrs={'class':'title', 'yalign' : ' '})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>') if tag.text != 'More' else '')
+        return(cnt)
     
-
- 
-  
+    cnt = extract_news('https://news.ycombinator.com/')
+    content += cnt
+    content += ('<br>------<br>') #End of body add email lines
+    content += ('<br><br>>End of Message')  #Add two lines the end of message line
+    
+    
+#sending an email
