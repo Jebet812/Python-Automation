52fa9a59f42e207d4a5d010f6341d35260504571
Jebet812 <cynthiajebet08@gmail.com>
2022-11-24 12:37:45 +0300

    Modified

--- image_editor.py
+++ image_editor.py
@@ -3,3 +3,10 @@
 from PIL import Image, ImageEnhance, ImageFilter
 
 import os
+
+path = 'imgs'
+pathOut = 'editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    
